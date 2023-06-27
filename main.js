f0e517df754f216916d11735fa7d26245e6b62f5
Eyal <eyalella@yahoo.com>
2023-06-27 18:18:16 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,96 +1,12 @@
-// Initialize variables
-let localConnection;
-let remoteConnection;
+var chatBox = document.getElementById("chat-box");
+var messageInput = document.getElementById("message-input");
+var sendMessageButton = document.getElementById("send-message");
 
-// Function to create a peer connection
-function createPeerConnection() {
-  const configuration = {
-    iceServers: [
-      { urls: 'stun:stun.l.google.com:19302' } // STUN server
-    ]
-  };
+var messages = [];
 
-  // Create local peer connection
-  localConnection = new RTCPeerConnection(configuration);
-
-  // Create data channel
-  localConnection.ondatachannel = event => {
-    const receiveChannel = event.channel;
-    receiveChannel.onmessage = handleMessageReceived;
-  };
-
-  // Create offer
-  localConnection.createOffer()
-    .then(offer => localConnection.setLocalDescription(offer))
-    .then(() => {
-      // Offer created, send it to remote peer through signaling server
-      const offerDescription = localConnection.localDescription;
-      // Send offerDescription to remote peer
-    })
-    .catch(error => console.error('Error creating offer:', error));
-}
-
-// Function to handle received message
-function handleMessageReceived(event) {
-  const message = event.data;
-  // Handle the received message
-}
-
-// Function to handle receiving an answer
-function handleAnswer(answerDescription) {
-  const answer = new RTCSessionDescription(answerDescription);
-  remoteConnection.setRemoteDescription(answer)
-    .catch(error => console.error('Error setting remote description:', error));
-}
-
-// Function to handle receiving an ICE candidate
-function handleCandidate(candidate) {
-  const iceCandidate = new RTCIceCandidate(candidate);
-  remoteConnection.addIceCandidate(iceCandidate)
-    .catch(error => console.error('Error adding ICE candidate:', error));
-}
-
-// Signaling server callback for receiving offers
-function handleOffer(offerDescription) {
-  remoteConnection = new RTCPeerConnection(configuration);
-
-  // Create data channel
-  const dataChannel = remoteConnection.createDataChannel('chatChannel');
-  dataChannel.onmessage = handleMessageReceived;
-
-  const offer = new RTCSessionDescription(offerDescription);
-  remoteConnection.setRemoteDescription(offer)
-    .then(() => remoteConnection.createAnswer())
-    .then(answer => remoteConnection.setLocalDescription(answer))
-    .then(() => {
-      // Answer created, send it to remote peer through signaling server
-      const answerDescription = remoteConnection.localDescription;
-      // Send answerDescription to remote peer
-    })
-    .catch(error => console.error('Error handling offer:', error));
-}
-
-// Signaling server callback for receiving answers
-function handleAnswer(answerDescription) {
-  const answer = new RTCSessionDescription(answerDescription);
-  localConnection.setRemoteDescription(answer)
-    .catch(error => console.error('Error setting remote description:', error));
-}
-
-// Signaling server callback for receiving ICE candidates
-function handleCandidate(candidate) {
-  const iceCandidate = new RTCIceCandidate(candidate);
-  localConnection.addIceCandidate(iceCandidate)
-    .catch(error => console.error('Error adding ICE candidate:', error));
-}
-
-// Set up signaling server connection (you need to implement this part)
-const signalingServer = new SignalingServer();
-
-// Signaling server callbacks
-signalingServer.onOffer = handleOffer;
-signalingServer.onAnswer = handleAnswer;
-signalingServer.onCandidate = handleCandidate;
-
-// Create peer connection when the user wants to start the chat
-createPeerConnection();
+sendMessageButton.onclick = function() {
+  var message = messageInput.value;
+  messages.push(message);
+  chatBox.innerHTML = messages.join("<br>");
+  messageInput.value = "";
+};
